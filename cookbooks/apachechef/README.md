c749b7da57d21ac4f8f5f1a722fec9851015ad7e
root <root@saiprasanth-rajavarapu-yahoo-in1.mylabserver.com>
2017-04-04 02:46:33 +0000

    added apache cookbook

---
+++ cookbooks/apachechef/README.md
@@ -0,0 +1,4 @@
+# apachechef
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/apachechef/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook:: apachechef
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+# Install apache package
+package 'apache2' do
+	package_name 'httpd'
+end
+
+service 'apache2' do
+	service_name 'httpd'
+	action [:enable, :start]
+end
+
